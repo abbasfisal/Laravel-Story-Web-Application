f9af6282041579bfcd0c5e146e719fdf76fd7d95
Abbas Momeni <abbasmomeny1994@gmail.com>
2022-07-10 22:47:23 +0430

    modify User Migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,6 +10,21 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+
+    /*
+     |------------------------------
+     | USER TYPES
+     |------------------------------
+     */
+    const TYPE_ADMIN = 'admin';
+    const TYPE_USER = 'user';
+    const TYPE = [
+        self::TYPE_ADMIN,
+        self::TYPE_USER
+    ];
+
+    //----------------------------------
 
     /**
      * The attributes that are mass assignable.
