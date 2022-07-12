3a4ffeced3167cc6e0c8777a07f71b0326e7f15d
Abbas Momeni <abbasmomeny1994@gmail.com>
2022-07-12 11:28:52 +0430

    feat user factory (create admin )

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +14,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->createAdmin();
+        
+    }
+
+    private function createAdmin()
+    {
+
+        $this->DeleteAdmin();
+
+        $this->call(UserSeeder::class);
+
+        $this->createAdminTableCommand();
+
+    }
+
+    private function DeleteAdmin(): void
+    {
+        $u = User::query()
+                 ->where([
+                     'name'     => 'admin',
+                     'email'    => 'admin@a.b',
+                     'username' => 'admin',
+                     'avatar'   => '',
+                 ])
+                 ->first();
+        $u->delete();
+    }
+
+    private function createAdminTableCommand(): void
+    {
+        $header = ['#', 'email', 'password', 'type'];
+        $body = ['#' => '1', 'email' => 'admin@a.b', 'password' => '123456', 'type' => 'admin'];
+
+        $this->command->table($header, [$body]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::query()
+            ->create([
+                'name'     => 'admin',
+                'email'    => 'admin@a.b',
+                'username' => 'admin',
+                'avatar'   => '',
+                'password' => Hash::make('123456'),
+            ]);
+    }
+}
